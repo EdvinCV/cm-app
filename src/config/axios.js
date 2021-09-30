152e1c9dd8eb78341a7731d5907093962f3be85b
cv <cv@cv.com>
2021-09-30 00:58:22 +0000

    Cambios axios

--- src/config/axios.js
+++ src/config/axios.js
@@ -3,7 +3,8 @@ import axios from 'axios';
 // Configuracion de cliente axios
 export const clientToken = axios.create({
     baseURL: 'http://67.205.180.171:3001/',
-    headers: {'Authorization': 'Token ' + localStorage.getItem("access_token")}
+    headers: {'Authorization': localStorage.getItem("access_token")}
+});
 
 export const client = axios.create({
     baseURL: 'http://67.205.180.171:3001/',
